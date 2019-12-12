9e38dc3e7bceab3a2e3b8d8bc9603ea9bc7ccc9c
Henry Vita <v02055020v@gmail.com>
2019-12-12 15:29:27 +0300

    Add Method go to Ghost
    TODO setNewCoord

--- pacman-be-application/GameManager/src/main/java/pacman/game_manager/model/Point.java
+++ pacman-be-application/GameManager/src/main/java/pacman/game_manager/model/Point.java
@@ -10,4 +10,26 @@ import lombok.NoArgsConstructor;
 public class Point {
     public Double x;
     public Double y;
+
+    public double getDistance(Point p) {
+        return Math.sqrt((p.x - x)*(p.x - x) + (p.y - y)*(p.y - y));
+    }
+
+    /** Return true if x and y is Int */
+    public boolean isCenter() {
+        if (Math.abs(x - DoubleToNearInt(x)) <= 0.001 &&
+                Math.abs(y - DoubleToNearInt(y)) <= 0.001) {
+            x = (double) DoubleToNearInt(x);
+            y = (double) DoubleToNearInt(y);
+            return true;
+        }
+        return false;
+    }
+
+    /** Rounding to the nearest */
+    public static int DoubleToNearInt(double d) {
+        if(d - (int)d >= 0.5) return (int)d + 1;
+        else return (int)d;
+    }
+
 }
